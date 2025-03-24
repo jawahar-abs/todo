46d733b8c636880b936eab440a7eb6a0b2327864
jawahar112 <bharathijawahar583@gmail.com>
2025-03-24 16:44:05 +0530

    Revert "Initial commit"
    This reverts commit c3ae5fedaedfc9c6df0c506304b62b2c5792d280.

--- src/store.js
+++
@@ -1,8 +0,0 @@
-import { TodoReducer,ThemeReducer } from "./reducers/Todoreducer";
-import {combineReducers, createStore } from "redux";
-export const rootReducer = combineReducers({
-    todoList:TodoReducer,
-    theme:ThemeReducer
-})
-const store = createStore(rootReducer)
-export default store
